7923317aabc2e2bcb9cb23fc9698a1cb24720045
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-11-09 20:22:18 +0600

    added admin service

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -3,15 +3,19 @@ import React, { useEffect, useState } from 'react';
 import useAuth from '../../../hooks/useAuth';
 
 const Appointments = ({ date }) => {
-    const { user } = useAuth();
+    const { user, token } = useAuth();
     const [appointments, setAppointments] = useState([]);
 
     useEffect(() => {
         const url = `http://localhost:5000/appointments?email=${user.email}&date=${date.toLocaleDateString()}`
-        fetch(url)
+        fetch(url, {
+            headers: {
+                'authorization': `Bearer ${token}`
+            }
+        })
             .then(res => res.json())
             .then(data => setAppointments(data));
-    }, [date])
+    }, [date, user.email, token])
 
     return (
         <div>
