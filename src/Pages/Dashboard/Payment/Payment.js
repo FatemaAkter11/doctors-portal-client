fdf9d5ebf97d973a9b3a5e0316749c81c19a2836
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-11-15 18:53:45 +0600

    Implement payment Process with simple card form

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useParams } from 'react-router';
+import { loadStripe } from '@stripe/stripe-js';
+import CheckoutForm from './CheckoutForm';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe('pk_test_51JvlRPCTYqqxvlaaeZik1gH4u6rS3gl2oPzPSjcOh7tZMeVJw2AZhiXeVmczGO48EuhhvGl42qWLgzfqFGZTwD1V00BMAgmmDB')
 
 const Payment = () => {
     const { appointmentId } = useParams();
@@ -15,6 +20,11 @@ const Payment = () => {
         <div>
             <h2>Please Pay for: {appointment.patientName} for {appointment.serviceName}</h2>
             <h4>Pay: ${appointment.price}</h4>
+            {appointment?.price && <Elements stripe={stripePromise}>
+                <CheckoutForm
+                    appointment={appointment}
+                />
+            </Elements>}
         </div>
     );
 };
